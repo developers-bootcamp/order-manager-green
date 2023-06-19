a97c30e5e71e561af0a7b4d8459ba18c0d7ac7c9
Sara Leah <slm3201386@gmail.com>
2023-06-20 00:57:37 +0300

    initial commit

---
+++ src/main/java/com/sap/ordermanegergreen/OrdersController.java
@@ -0,0 +1,17 @@
+package com.sap.ordermanegergreen;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+
+public class OrdersController {
+
+    @GetMapping
+    public String getOrders(){
+    return "YAY!!!";
+    }
+}
