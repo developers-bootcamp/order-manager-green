112564a655cc55127f2bd75fdd2f3247895242d6
chanirozenboim <136738736+chanirozenboim@users.noreply.github.com>
2023-06-25 19:27:22 +0300

    login-api with moc data

--- src/main/java/com/sap/ordermanegergreen/Services/UserService.java
+++ src/main/java/com/sap/ordermanegergreen/Services/UserService.java
@@ -3,7 +3,13 @@ package com.sap.ordermanegergreen.Services;
 import com.sap.ordermanegergreen.Models.User;
 import com.sap.ordermanegergreen.Repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.time.LocalDate;
+import java.util.Date;
 
 import java.util.List;
 
@@ -37,6 +43,30 @@ public class UserService implements IUserService {
 
     public void deletebyId(String id) {
         userRepository.deleteById(id);
+    }
+
+    public boolean isEmailExists(String email){
+        //TODO: replace mockData with real call - check if email exists in the db
+        // userRepository.isUserExists (by email)
+        return true;
+    }
+
+    @Override
+    public User getUserByEmailAndPassword(String email, String password) {
+        if (Boolean.FALSE.equals(isEmailExists(email))) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND,"User not found. Please sign up" );
+        }
+        //TODO: replace mockData with real call - userRepository.getUserByEmailAnsPassword
+        User user = new User("1","Chani","111"
+                ,new Address("3527453746","Rabi Akiva","aaa@nnn.com")
+                ,new Roles("2222", AvailableRoles.ADMIN,"roleDesc",new AuditData())
+                ,new Company("3333","comp", "ILS", new AuditData())
+                ,new AuditData(new Date(), new Date()));
+        if (user == null) {
+            throw new ResponseStatusException(HttpStatus.UNAUTHORIZED,"Invalid credentials" );
+        }
+
+        return user;
     }
 
 }
