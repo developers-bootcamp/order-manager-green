1e7986603d4064bfbdff84e7b3dd5a6c5c71170d
ch5706802 <136978389+ch5706802@users.noreply.github.com>
2023-07-05 17:09:20 +0300

    Update CustomerController.java

--- src/main/java/com/sap/ordermanegergreen/controller/CustomerController.java
+++ src/main/java/com/sap/ordermanegergreen/controller/CustomerController.java
@@ -1,7 +1,11 @@
 package com.sap.ordermanegergreen.controller;
 
+import com.sap.ordermanegergreen.dto.TokenDTO;
+import com.sap.ordermanegergreen.model.AvailableRoles;
+import com.sap.ordermanegergreen.model.Company;
 import com.sap.ordermanegergreen.model.User;
 import com.sap.ordermanegergreen.service.CustomerService;
+import com.sap.ordermanegergreen.util.JwtToken;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -10,37 +14,45 @@ import java.util.List;
 @RestController
 @RequestMapping("/customer")
 public class CustomerController {
-    
+
     private CustomerService customerService;
-    
+    private JwtToken jwtToken;
+
     @Autowired
-    public CustomerController(CustomerService customerService) {
+    public CustomerController(CustomerService customerService, JwtToken jwtToken) {
+        this.jwtToken = jwtToken;
         this.customerService = customerService;
     }
 
     @GetMapping
-    public List<User> getAll() {
-        return customerService.getAll();
+    @RequestMapping("/getToken/{token}")
+    public TokenDTO getToken(@PathVariable String token) {
+        return jwtToken.decodeToken(token);
     }
 
-   @GetMapping("/{id}")
-   public User getById(@PathVariable String id) {
-       return customerService.getById(id);
-   }
+    @GetMapping
+    public List<User> getAll() {
+        return customerService.getAll().stream().filter(c->c.getRoleId().equals(AvailableRoles.CUSTOMER)).toList();
+    }
 
-   @PostMapping
-   public void add(@RequestBody User user) {
-       customerService.add(user);
-   }
+    @GetMapping("/{id}")
+    public User getById(@PathVariable String id) {
+        return customerService.getById(id);
+    }
 
-   @PutMapping("/{id}")
-   public User editById(@RequestBody User user, @PathVariable String id) {
-       return customerService.put(user, id);
-   }
+    @PostMapping
+    public void add(@RequestBody User user) {
+        customerService.add(user);
+    }
 
-   @DeleteMapping("/{id}")
-   public void deleteById(@PathVariable String id) {
-       customerService.deletebyId(id);
-   }
-    
+    @PutMapping("/{id}")
+    public User editById(@RequestBody User user, @PathVariable String id) {
+        return customerService.put(id, user);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteById(@PathVariable String id) {
+        customerService.deletebyId(id);
+    }
+
 }
