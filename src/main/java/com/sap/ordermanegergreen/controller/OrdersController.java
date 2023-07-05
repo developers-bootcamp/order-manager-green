8e444e9cd91503b190bb09e9e3ba1d8a880e83a0
ch5706802 <136978389+ch5706802@users.noreply.github.com>
2023-07-05 17:23:38 +0300

    Update OrdersController.java

--- src/main/java/com/sap/ordermanegergreen/controller/OrdersController.java
+++ src/main/java/com/sap/ordermanegergreen/controller/OrdersController.java
@@ -4,8 +4,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@CrossOrigin("http://localhost:3000")
 @RestController
-@RequestMapping("/Orders")
+@RequestMapping("/orders")
 public class OrdersController {
     @GetMapping
     @RequestMapping("/getOrders")
