6a53c6ad3e48d75c7069f56bc30bd1e153b02603
ch5706802 <136978389+ch5706802@users.noreply.github.com>
2023-07-05 16:07:04 +0300

    Update IUserRepository.java

--- src/main/java/com/sap/ordermanegergreen/repository/IUserRepository.java
+++ src/main/java/com/sap/ordermanegergreen/repository/IUserRepository.java
@@ -1,10 +1,9 @@
 package com.sap.ordermanegergreen.repository;
 
 import com.sap.ordermanegergreen.model.User;
-import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Component;
-    @Component
-    public interface IUserRepository extends MongoRepository<User, String> {
-    }
-
-
+import org.springframework.data.mongodb.repository.MongoRepository;
+    
+@Component
+public interface IUserRepository extends MongoRepository<User, String> {
+}
