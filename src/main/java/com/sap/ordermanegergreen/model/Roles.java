c839b71fccbc7d6b8606b668d63a66c5b4f60955
ch5706802 <136978389+ch5706802@users.noreply.github.com>
2023-07-05 15:51:46 +0300

    Update Roles.java

--- src/main/java/com/sap/ordermanegergreen/model/Roles.java
+++ src/main/java/com/sap/ordermanegergreen/model/Roles.java
@@ -1,28 +1,24 @@
 package com.sap.ordermanegergreen.model;
 
-import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
-;
-//@Document לשנות שם לטבלה
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-@Document(collection = "Roles")
+@Document(collection = "Roles")  // לשנות שם לטבלה
 public class Roles {
+    
     @Id
     private String id;
+    
     private AvailableRoles name;
+    
     private String desc;
+    
     private AuditDate auditData;
-    public String getId() {
-        return id;
-    }
-
-    public void setId(String id) {
-        this.id = id;
-    }
+    
 }
