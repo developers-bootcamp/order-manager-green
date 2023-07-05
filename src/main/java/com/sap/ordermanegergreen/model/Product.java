ecbd3b2d2e36b91df637fd8807bc59598c6cd1a4
ch5706802 <136978389+ch5706802@users.noreply.github.com>
2023-07-05 15:44:34 +0300

    Update Product.java

--- src/main/java/com/sap/ordermanegergreen/model/Product.java
+++ src/main/java/com/sap/ordermanegergreen/model/Product.java
@@ -1,30 +1,38 @@
 package com.sap.ordermanegergreen.model;
 
-import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.DBRef;
 import org.springframework.data.mongodb.core.mapping.Document;
 
-;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-//@Collation
 @Document(collection = "Product")
 public class Product {
-    @Id private String id;
+    
+    @Id 
+    private String id;
+    
     private String name;
+    
     private String description;
+    
     private  double price;
+    
     private int discount;
+    
     private DiscountTypes discountType;
-    //@DBRef
+    
     private String categoryId;
+    
     private int inventory;
-    //@DBRef
-    private String companyId;
+    
     @DBRef
+    private String companyId;
+    
     private AuditDate auditData;
+    
 }
