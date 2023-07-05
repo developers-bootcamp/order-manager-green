f256f158c1118a8e9805857cbee356432948bab2
ch5706802 <136978389+ch5706802@users.noreply.github.com>
2023-07-05 17:10:26 +0300

    Update OrdersController.java

--- src/main/java/com/sap/ordermanegergreen/controller/OrdersController.java
+++ src/main/java/com/sap/ordermanegergreen/controller/OrdersController.java
@@ -4,48 +4,13 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-
 @RestController
-@RequestMapping("/orders")
+@RequestMapping("/Orders")
 public class OrdersController {
-    
-    private OrdersService ordersService;
-    private JwtToken jwtToken;
-    
-    @Autowired
-    public OrdersController(OrdersService ordersService,JwtToken jwtToken) {
-        this.jwtToken=jwtToken;
-        this.ordersService = ordersService;
-    }
-    
     @GetMapping
-    @RequestMapping("/getToken/{token}")
-    public TokenDTO getToken(@PathVariable String token) {
-        return jwtToken.decodeToken(token);
-    }
-    
-    @GetMapping
-    public List<Order> getAll() {
-        return ordersservice.getAll();
-    }
-
-    @GetMapping("/{id}")
-    public Company getById(@PathVariable String id) {
-        return companyService.getById(id);
-    }
-
-    @PostMapping
-    public void add(@RequestBody Company company) {
-        companyService.add(company);
-    }
-
-    @PutMapping("/{id}")
-    public Company editById(@RequestBody Company company, @PathVariable String id) {
-        return companyService.editById(company, id);
-    }
-
-    @DeleteMapping("/{id}")
-    public void deleteById(@PathVariable String id) {
-        companyService.deletebyId(id);
+    @RequestMapping("/getOrders")
+    public String getOrders(){
+        
+    return "YAY!!!";
     }
 }
