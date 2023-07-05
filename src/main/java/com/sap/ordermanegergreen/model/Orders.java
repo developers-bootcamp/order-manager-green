60334577188bd5446527716e1b376adb6d53e9e6
ch5706802 <136978389+ch5706802@users.noreply.github.com>
2023-07-05 15:39:07 +0300

    Update Orders.java

--- src/main/java/com/sap/ordermanegergreen/model/Orders.java
+++ src/main/java/com/sap/ordermanegergreen/model/Orders.java
@@ -1,8 +1,8 @@
 package com.sap.ordermanegergreen.model;
 
-import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.DBRef;
 import org.springframework.data.mongodb.core.mapping.Document;
@@ -10,24 +10,37 @@ import org.springframework.data.mongodb.core.mapping.Document;
 import java.util.List;
 
 @Data
-@Document(collection = "Orders")
 @NoArgsConstructor
 @AllArgsConstructor
-//@ToString
+@Document(collection = "Orders")
 public class Orders {
- @Id private String id;
-   @DBRef
-   private String employeeId ;
+ 
+    @Id 
+    private String id;
+ 
+    @DBRef
+    private String employeeId ;
+ 
     @DBRef
     private String customerId;
+ 
     private double totalAmount;
+ 
     private List<OrderItems> orderItemsList;
+ 
     private String orderStatusId;
+ 
     @DBRef
     private String companyId;
+ 
     private String creditCardNumber;
+ 
     private String  expiryOn;
+ 
     private String cvc;
+ 
     private Boolean notificationFlag;
+ 
     private AuditDate auditDate;
+ 
 }
