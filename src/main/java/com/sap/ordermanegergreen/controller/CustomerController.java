b5c2687d83caf39723d54a1656e53c4610dd9efd
ch5706802 <136978389+ch5706802@users.noreply.github.com>
2023-07-05 15:18:49 +0300

    Update CustomerController.java

--- src/main/java/com/sap/ordermanegergreen/controller/CustomerController.java
+++ src/main/java/com/sap/ordermanegergreen/controller/CustomerController.java
@@ -10,40 +10,36 @@ import java.util.List;
 @RestController
 @RequestMapping("/customer")
 public class CustomerController {
+    
     private CustomerService customerService;
+    
     @Autowired
     public CustomerController(CustomerService customerService) {
         this.customerService = customerService;
     }
 
     @GetMapping
-    @RequestMapping("/getAll")
     public List<User> getAll() {
         return customerService.getAll();
     }
-//becouse user include it...
 
-//    @GetMapping
-//    @RequestMapping("/getById/{id}")
-//    public User getById(@PathVariable String id) {
-//        return customerService.getById(id);
-//    }
-//
-//    @PostMapping
-//    @RequestMapping("/add")
-//    public void add(@RequestBody User p) {
-//        customerService.add(p);
-//    }
+   @GetMapping("/{id}")
+   public User getById(@PathVariable String id) {
+       return customerService.getById(id);
+   }
 
-//    @PutMapping
-//    @RequestMapping("/editById/{id}")
-//    public User editById(@RequestBody User user, @PathVariable String id) {
-//        return customerService.put(user, id);
-//    }
-//
-//    @DeleteMapping
-//    @RequestMapping("/deleteById/{id}")
-//    public void deleteById(@PathVariable String id) {
-//        customerService.deletebyId(id);
-//    }
+   @PostMapping
+   public void add(@RequestBody User user) {
+       customerService.add(user);
+   }
+
+   @PutMapping("/{id}")
+   public User editById(@RequestBody User user, @PathVariable String id) {
+       return customerService.put(user, id);
+   }
+
+   @DeleteMapping("/{id}")
+   public void deleteById(@PathVariable String id) {
+       customerService.deletebyId(id);
+   }
 }
