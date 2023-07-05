962fc43cb67dc9fd87dec102574185039dc4c188
ch5706802 <136978389+ch5706802@users.noreply.github.com>
2023-07-05 16:05:02 +0300

    Update IProductCategoryRepository.java

--- src/main/java/com/sap/ordermanegergreen/repository/IProductCategoryRepository.java
+++ src/main/java/com/sap/ordermanegergreen/repository/IProductCategoryRepository.java
@@ -1,11 +1,12 @@
 package com.sap.ordermanegergreen.repository;
 
+import org.springframework.stereotype.Component;
 import com.sap.ordermanegergreen.model.ProductCategory;
 import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.stereotype.Component;
 
 @Component
 public interface IProductCategoryRepository extends MongoRepository<ProductCategory, String> {
+    
     boolean existsByName(String categoryName);
 
 }
