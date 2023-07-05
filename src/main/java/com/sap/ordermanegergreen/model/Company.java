dd6eb1c045833d76186c187c236cf469e2c98fb5
ch5706802 <136978389+ch5706802@users.noreply.github.com>
2023-07-05 15:56:02 +0300

    Update Company.java

--- src/main/java/com/sap/ordermanegergreen/model/Company.java
+++ src/main/java/com/sap/ordermanegergreen/model/Company.java
@@ -1,8 +1,6 @@
 package com.sap.ordermanegergreen.model;
 
-import lombok.Data;
-import lombok.NoArgsConstructor;
-import lombok.AllArgsConstructor;
+import lombok.*;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.DBRef;
 import org.springframework.data.mongodb.core.mapping.Document;
