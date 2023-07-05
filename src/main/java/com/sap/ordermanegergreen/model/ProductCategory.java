2c18f6dbfdf27be8df5157e704f91a5e70a1223b
ch5706802 <136978389+ch5706802@users.noreply.github.com>
2023-07-05 16:42:52 +0300

    Update ProductCategory.java

--- src/main/java/com/sap/ordermanegergreen/model/ProductCategory.java
+++ src/main/java/com/sap/ordermanegergreen/model/ProductCategory.java
@@ -2,6 +2,7 @@ package com.sap.ordermanegergreen.model;
 
 import lombok.*;
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.DBRef;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @Data
