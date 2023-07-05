4538eed7a584808c8bca96461197fbbace80e3a6
ch5706802 <136978389+ch5706802@users.noreply.github.com>
2023-07-05 15:35:50 +0300

    Update Address.java

--- src/main/java/com/sap/ordermanegergreen/model/Address.java
+++ src/main/java/com/sap/ordermanegergreen/model/Address.java
@@ -1,14 +1,18 @@
 package com.sap.ordermanegergreen.model;
 
-import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.AllArgsConstructor;
 import lombok.NoArgsConstructor;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class Address {
+    
     private String telephone;
+    
     private String address;
+    
     private String email;
+    
 }
