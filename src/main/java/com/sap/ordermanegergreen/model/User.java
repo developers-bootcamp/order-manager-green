bd22fbcf2c80a7fbabd0588ba3944636063dbe93
ch5706802 <136978389+ch5706802@users.noreply.github.com>
2023-07-05 15:53:02 +0300

    Update User.java

--- src/main/java/com/sap/ordermanegergreen/model/User.java
+++ src/main/java/com/sap/ordermanegergreen/model/User.java
@@ -10,14 +10,22 @@ import org.springframework.data.mongodb.core.mapping.Document;
 @AllArgsConstructor
 @Document(collection = "Users")
 public class User {
+    
     @Id
     private String id;
+    
     private String fullName;
+    
     private String password;
+    
     private Address addressId;
+    
     @DBRef
     private String roleId;
+    
     @DBRef
     private String companyId;
+    
     private AuditDate auditData;
+    
 }
