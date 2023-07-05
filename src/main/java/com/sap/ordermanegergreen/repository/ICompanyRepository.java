2a72e7b34920f31eb73675a5fdbad3186143a303
ch5706802 <136978389+ch5706802@users.noreply.github.com>
2023-07-05 15:57:54 +0300

    Update ICompanyRepository.java

--- src/main/java/com/sap/ordermanegergreen/repository/ICompanyRepository.java
+++ src/main/java/com/sap/ordermanegergreen/repository/ICompanyRepository.java
@@ -1,11 +1,9 @@
 package com.sap.ordermanegergreen.repository;
 import com.sap.ordermanegergreen.model.Company;
-import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Component;
+import org.springframework.data.mongodb.repository.MongoRepository;
 
 
-    @Component
-    public interface ICompanyRepository extends MongoRepository<Company, String> {
-    }
-
-
+@Component
+public interface ICompanyRepository extends MongoRepository<Company, String> {
+}
