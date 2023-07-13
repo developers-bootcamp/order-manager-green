ae86c00e492c64824057d26713378a312c7cdb37
ch5706802 <136978389+ch5706802@users.noreply.github.com>
2023-07-13 20:27:42 +0300

    Update UserService.java

--- src/main/java/com/sap/ordermanagergreen/service/UserService.java
+++ src/main/java/com/sap/ordermanagergreen/service/UserService.java
@@ -1,27 +1,33 @@
 package com.sap.ordermanagergreen.service;
 
-import com.sap.ordermanagergreen.model.*;
-import org.springframework.stereotype.Service;
-import com.sap.ordermanagergreen.repository.IUserRepository;
-import org.springframework.beans.factory.annotation.Autowired;
+import com.sap.ordermanagergreen.exception.NoPremissionException;
 import com.sap.ordermanagergreen.exception.NotValidException;
 import com.sap.ordermanagergreen.exception.ObjectExistException;
+import com.sap.ordermanagergreen.repository.ICompanyRepository;
+import com.sap.ordermanagergreen.repository.IRoleRepository;
+import com.sap.ordermanagergreen.repository.IUserRepository;
+import com.sap.ordermanagergreen.model.*;
+import com.sap.ordermanagergreen.dto.TokenDTO;
+import com.sap.ordermanagergreen.util.JwtToken;
+import lombok.SneakyThrows;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.Date;
-import java.util.List;
 
 @Service
 public class UserService {
-
-    private CompanyService companyService;
-    private RoleService rolesService;
     IUserRepository userRepository;
+    IRoleRepository roleRepository;
+    ICompanyRepository companyRepository;
 
     @Autowired
-    public UserService(IUserRepository userRepository, CompanyService companyService, RoleService rolesService) {
+    public UserService(IUserRepository userRepository, IRoleRepository roleRepository, ICompanyRepository companyRepository) {
         this.userRepository = userRepository;
-        this.companyService = companyService;
-        this.rolesService = rolesService;
+        this.roleRepository = roleRepository;
+        this.companyRepository = companyRepository;
     }
 
     public User signUp(String fullName, String companyName, String email, String password) throws Exception {
@@ -43,17 +49,17 @@ public class UserService {
             Address address = new Address();
             user.setAddress(address);
             user.getAddress().setEmail(email);
-            user.setRoleId(rolesService.getByName(AvailableRoles.ADMIN));
+            user.setRoleId(roleRepository.getByName(AvailableRoles.ADMIN));
             AuditData auditData = new AuditData();
             auditData.setCreateDate(new Date());
             auditData.setUpdateDate(new Date());
             user.setAuditData(auditData);
-            if (companyService.existsByName(companyName)) {
+            if (companyRepository.existsByName(companyName)) {
                 throw new ObjectExistException("company");
             }
             Company company = new Company();
             company.setName(companyName);
-            companyService.add(company);
+            companyRepository.save(company);
             AuditData auditData1 = new AuditData();
             auditData1.setCreateDate(new Date());
             auditData1.setUpdateDate(new Date());
@@ -64,4 +70,56 @@ public class UserService {
         } catch (Exception e) {
             throw new Exception();
         }
-    }}
+    }
+
+    @SneakyThrows
+    public void add(String token, User user) {
+        if (userRepository.existsByFullName(user.getFullName())) {
+            throw new ObjectExistException("user name already exist");
+        }
+        TokenDTO tokenDTO = JwtToken.decodeToken(token);
+        if (roleRepository.findById(tokenDTO.getRoleId()).orElse(new Role()).getName() != AvailableRoles.CUSTOMER) {
+            user.setAuditData(new AuditData(new Date(), new Date()));
+            userRepository.save(user);
+        }
+    }
+
+    @SneakyThrows
+    public void deleteById(String token, String userId) {
+        TokenDTO tokenDTO = JwtToken.decodeToken(token);
+        if (roleRepository.findById(tokenDTO.getRoleId()).orElse(new Role()).getName() == AvailableRoles.CUSTOMER || !(companyRepository.findById(tokenDTO.getCompanyId()).orElse(new Company()).getId().equals(userRepository.findById(userId).orElse(new User()).getCompanyId().getId()))) {
+            throw new NoPremissionException("You don't have permission to delete the user");
+        }
+        if (userRepository.findById(userId).isEmpty()) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "User does not exist");
+        }
+        userRepository.deleteById(userId);
+    }
+
+    @SneakyThrows
+    public void editById(String token, User user) {
+        TokenDTO tokenDTO = JwtToken.decodeToken(token);
+        if (roleRepository.findById(tokenDTO.getRoleId()).orElse(new Role()).getName() == AvailableRoles.CUSTOMER || !(companyRepository.findById(tokenDTO.getCompanyId()).orElse(new Company()).getId().equals(userRepository.findById(user.getId()).orElse(new User()).getCompanyId().getId()))) {
+            throw new NoPremissionException("You don't have permission to delete the user");
+        }
+        if (userRepository.findById(user.getId()).isEmpty()) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "User does not exist");
+        }
+        userRepository.save(user);
+    }
+
+    public User isEmailExists(String email) {
+        return userRepository.findByAddress_Email(email);
+    }
+
+    public User getUserByEmailAndPassword(String userEmail, String userPassword) {
+        User user = isEmailExists(userEmail);
+        if (user == null) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "User not found. Please sign up");
+        } else {
+            if (!user.getPassword().equals(userPassword))
+                throw new ResponseStatusException(HttpStatus.UNAUTHORIZED, "Invalid credentials");
+            return user;
+        }
+    }
+}
