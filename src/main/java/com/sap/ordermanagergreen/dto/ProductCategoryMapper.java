e0451cfd00fe1b062cb16a0f8fbf620a7cff1afb
fraidy1 <136713067+fraidy1@users.noreply.github.com>
2023-07-24 15:49:05 +0300

    Product category api (#28)
    * productCategory api
    
    * productCategory api
    
    * productCategory api

---
+++ src/main/java/com/sap/ordermanagergreen/dto/ProductCategoryMapper.java
@@ -0,0 +1,29 @@
+package com.sap.ordermanagergreen.dto;
+
+import com.sap.ordermanagergreen.model.ProductCategory;
+import org.mapstruct.Mapper;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Mapper(componentModel = "spring")
+public interface ProductCategoryMapper {
+
+
+
+    ProductCategoryDTO toDto(ProductCategory productCategory);
+    ProductCategory fromDto(ProductCategoryDTO productCategoryDTO);
+
+
+    default List<ProductCategory> fromDtoList(List<ProductCategoryDTO> productCategoryDTOs) {
+        return productCategoryDTOs.stream()
+                .map(this::fromDto)
+                .collect(Collectors.toList());
+    }
+    default List<ProductCategoryDTO> toDtoList(List<ProductCategory> productCategories) {
+        return productCategories.stream()
+                .map(this::toDto)
+                .collect(Collectors.toList());
+    }
+}
+
