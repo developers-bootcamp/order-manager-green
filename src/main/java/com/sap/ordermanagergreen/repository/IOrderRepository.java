b5a6fe24343e93daa3ad1ce2f137585a8e7526f2
Moriya <35287680+moriyahazan@users.noreply.github.com>
2023-08-13 02:59:58 +0300

    top product

--- src/main/java/com/sap/ordermanagergreen/repository/IOrderRepository.java
+++ src/main/java/com/sap/ordermanagergreen/repository/IOrderRepository.java
@@ -1,81 +1,15 @@
 package com.sap.ordermanagergreen.repository;
 
-import com.mongodb.client.MongoClient;
-import com.mongodb.client.MongoDatabase;
-import com.sap.ordermanagergreen.dto.TopProductDTO;
-import com.sap.ordermanagergreen.model.OrderStatus;
 import com.sap.ordermanagergreen.model.Order;
-import com.sap.ordermanagergreen.model.Month;
-
-
-import org.springframework.data.mongodb.repository.MongoRepository;
+import com.sap.ordermanagergreen.model.OrderStatus;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.*;
-import java.util.stream.Collectors;
-
-import org.bson.Document;
-import com.sap.ordermanagergreen.dto.ProductCountDto;
-
-import com.mongodb.client.MongoCollection;
-import com.mongodb.client.AggregateIterable;
+import java.util.List;
 
 @Repository
 public interface IOrderRepository extends MongoRepository<Order, String> {
     List<Order> findByOrderStatusAndCompany_Id(Pageable pageable, OrderStatus orderStatusId, String companyName);
 
-    //    @Query()
-    default List<TopProductDTO> getTopProductsGroupedByMonth(Date fromMonth, Date untilMonth) {
-        MongoClient mongoClient = new MongoClient(mongodb);
-        MongoDatabase database = mongoClient.getDatabase("Orders");
-        MongoCollection<Document> collection = database.getCollection("Orders");
-
-        AggregateIterable<Document> result = getcollection.aggregate(Arrays.asList(
-                new Document("$match",
-                        new Document("audit_data.create_date",
-                                new Document("$gte", fromMonth)
-                                        .append("$lt", untilMonth))),
-                new Document("$unwind",
-                        new Document("path", "$order_items")),
-                new Document("$project",
-                        new Document("month",
-                                new Document("$month",
-                                        new Document("$toDate", "$audit_data.create_date")))
-                                .append("product", "$order_items.product_id")
-                                .append("quantity", "$order_items.quantity")),
-                new Document("$group",
-                        new Document("_id",
-                                new Document("month", "$month")
-                                        .append("product", "$product"))
-                                .append("totalQuantity",
-                                        new Document("$sum", "$quantity"))),
-                new Document("$sort",
-                        new Document("totalQuantity", -1L)),
-                new Document("$group",
-                        new Document("_id", "$_id.month")
-                                .append("products",
-                                        new Document("$push",
-                                                new Document("product", "$_id.product")
-                                                        .append("totalQuantity", "$totalQuantity"))))));
-
-        List<TopProductDTO> topProducts = new ArrayList<>();
-        for (Document document : result) {
-            int month = document.getInteger("_id");
-            List<Document> products = document.get("products", List.class);
-            List<ProductCountDto> productCountList = products.stream()
-                    .map(productDocument -> {
-                        String productName = productDocument.getString("product");
-                        int count = productDocument.getInteger("totalQuantity");
-                        return new ProductCountDto(productName, count);
-                    })
-                    .collect(Collectors.toList());
-            int currentYear = Calendar.getInstance().get(Calendar.YEAR);
-            Month lastMonth = new Month(currentYear,month);
-            TopProductDTO topProductDTO = new TopProductDTO(lastMonth, productCountList);
-            topProducts.add(topProductDTO);
-        }
-
-        return topProducts;
-    }
 }
--- src/main/java/com/sap/ordermanagergreen/service/GraphService.java
+++ src/main/java/com/sap/ordermanagergreen/service/GraphService.java
@@ -1,8 +1,15 @@
 package com.sap.ordermanagergreen.service;
 
+import com.mongodb.client.AggregateIterable;
+import com.mongodb.client.MongoCollection;
 import com.mongodb.client.model.Projections;
-import com.sap.ordermanagergreen.model.User;
+import com.sap.ordermanagergreen.dto.ProductCountDto;
+import com.sap.ordermanagergreen.dto.TopProductDTO;
+import com.sap.ordermanagergreen.model.*;
 import com.sap.ordermanagergreen.repository.IOrderRepository;
+import com.sap.ordermanagergreen.repository.IProductCategoryRepository;
+import com.sap.ordermanagergreen.repository.IProductRepository;
+import org.bson.Document;
 import org.bson.conversions.Bson;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Sort;
@@ -12,13 +19,12 @@ import org.springframework.expression.spel.ast.Projection;
 import org.springframework.stereotype.Service;
 import org.springframework.data.mongodb.core.MongoTemplate;
 
-import javax.swing.text.Document;
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.time.Year;
-import java.util.Date;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
+import java.util.Currency;
+import java.util.stream.Collectors;
 
 import static org.springframework.data.mongodb.core.aggregation.Aggregation.*;
 
@@ -26,8 +32,59 @@ import static org.springframework.data.mongodb.core.aggregation.Aggregation.*;
 public class GraphService {
 
     @Autowired
-    private IOrderRepository orderRepository;
-    @Autowired
     private MongoTemplate mongoTemplate;
+
+
+    public List<TopProductDTO> getTopProductsGroupedByMonth(LocalDate fromMonth, LocalDate untilMonth) {
+        MongoCollection<org.bson.Document> orderCollection = mongoTemplate.getCollection("Order");
+
+
+        AggregateIterable<org.bson.Document> result = orderCollection.aggregate(Arrays.asList(
+           /*     new org.bson.Document("$match",
+                        new org.bson.Document("auditData.createDate",
+                                new org.bson.Document("$gte", fromMonth)
+                                        .append("$lt", untilMonth))),*/
+                new org.bson.Document("$unwind",
+                        new org.bson.Document("path", "orderItemsList")),
+                new org.bson.Document("$project",
+                        new org.bson.Document("month",
+                                new org.bson.Document("$month",
+                                        new org.bson.Document("$toDate", "$auditData.createDate")))
+                                .append("product", "orderItemsList.product_id")
+                                .append("quantity", "orderItemsList.quantity")),
+                new org.bson.Document("$group",
+                        new org.bson.Document("_id",
+                                new org.bson.Document("month", "$month")
+                                        .append("product", "$product"))
+                                .append("totalQuantity",
+                                        new org.bson.Document("$sum", "$quantity"))),
+                new org.bson.Document("$sort",
+                        new org.bson.Document("totalQuantity", -1L)),
+                new org.bson.Document("$group",
+                        new org.bson.Document("_id", "$_id.month")
+                                .append("products",
+                                        new org.bson.Document("$push",
+                                                new org.bson.Document("product", "$_id.product")
+                                                        .append("totalQuantity", "$totalQuantity"))))));
+
+        List<TopProductDTO> topProducts = new ArrayList<>();
+        for (org.bson.Document document : result) {
+            int month = document.getInteger("_id");
+            List<Document> products = document.get("products", List.class);
+            List<ProductCountDto> productCountList = products.stream()
+                    .map(productDocument -> {
+                        String productName = productDocument.getString("product");
+                        int count = productDocument.getInteger("totalQuantity");
+                        return new ProductCountDto(productName, count);
+                    })
+                    .collect(Collectors.toList());
+            int currentYear = Calendar.getInstance().get(Calendar.YEAR);
+            Month lastMonth = new Month(currentYear, month);
+            TopProductDTO topProductDTO = new TopProductDTO(lastMonth, productCountList);
+            topProducts.add(topProductDTO);
+        }
+
+        return topProducts;
+    }
 }
 
