a8285e24c0bd785ac4a2a00339963a91e95fc097
Sara Leah <109041779+sara1386@users.noreply.github.com>
2023-08-17 19:13:45 +0300

    top product aggregation (#27)
    * top product aggregation
    
    * delete dto
    
    * product changes
    
    * customer, company, user - base api
    
    * login-api with moc data
    
    * change1
    
    * compile error at top product aggregation
    
    * top product
    
    * change "Month" model to "MonthInYear"
    
    * change "Month" model to "MonthInYear" nd fix copilation error
    
    ---------
    
    Co-authored-by: avigailRo <136953237+avigailRo@users.noreply.github.com>
    Co-authored-by: ch5706802 <136978389+ch5706802@users.noreply.github.com>
    Co-authored-by: chanirozenboim <136738736+chanirozenboim@users.noreply.github.com>
    Co-authored-by: Moriya <35287680+moriyahazan@users.noreply.github.com>

--- src/main/java/com/sap/ordermanagergreen/model/AuditData.java
+++ src/main/java/com/sap/ordermanagergreen/model/AuditData.java
@@ -15,7 +15,7 @@ public class AuditData {
   private LocalDateTime createDate;
   private LocalDateTime updateDate;
   public AuditData(){
-    this.createDate=LocalDateTime.now();
-    this.updateDate=LocalDateTime.now();
+    this.createDate = LocalDateTime.now();
+    this.updateDate = LocalDateTime.now();
   }
 }
